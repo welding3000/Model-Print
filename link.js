d7eb864c16118bdc445f3ac1901f97cc34692b56
Miguel <welding3000@users.noreply.github.com>
2016-12-15 04:40:39 -0500

    Create link.js

---
+++ link.js
@@ -0,0 +1,209 @@
+<!--###------------------------Assign.html------------------------------------------------###-->
+
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvem1yTy0teXJHVTQ"> links </a>
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvOHJLenFvNFVNNmM"> links1 </a>
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvamQ1M0N1S1BXM0U"> links2 </a>
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvUjlQX1FGeWFrWTA"> links3 </a>
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvbGw4cVZHSU9OVU0"> links4 </a>
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvOExFdDBnQTZZZ1E"> links5 </a>
+
+<!--###---------------Comments.html---------------------------------------------------------###-->
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvS0k0dVlac2hwMnc"> link </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvOEwxU2lSU3RudEk"> link1 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvT3VtR0xlcWZWaGc"> link2 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvV1hrdzNQOXVET00"> link3 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvWkN2VmNoTko5dEU"> link4 </a>
+
+<!--###----------------Datatypes.html--------------------------------------------------------###-->
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvaENXMWU3SXhMYlk"> links1 </a>
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvcTg5QVVkMkg5ekU"> links2 </a>
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvMTlHX3ZkY3NRWW8"> links3 </a>
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvcFhrLS1uTXlJN2M"> links4 </a>
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvN3JRbFIwXzBtakk"> links5 </a>
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvdUJFVXVOZDFkODA"> links6 </a>
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvd3MwLXRWMFZyeDg"> links7 </a>
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvckdHRVJURElXZFU"> links8 </a>
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvM1E0b1ZvWGUyWEk"> links9 </a>
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvaFh2S2ZIMlZPY1U"> links10 </a>
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvY1JFQlY5QVN1OVk"> links11 </a>
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvUk9CbG9fb1o4cXM"> links12 </a>
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvd19fRkdHUlRfVHM"> links13 </a>
+
+<!--###----------------date.html--------------------------------------------------------###-->
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvT2V6eWRmd0h3eTQ"> links2 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvdV9UZ3RhYTJwblE"> links3 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvazh1VGE0bE4wUEk"> links4 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvOWdyN1liVkdRQTA"> links5 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvYk54UENIMHA3Z2s"> links6 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvUE1oMTlydEV6Wkk"> links7 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvUUN1a0tfSkF4ekU"> links8 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvN2xmTUtEcGFOYlk"> links9 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvWmp5YWRGaXowNjg"> links10 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvdGNZNjJ3eUl1VVE"> links1 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvaERVS0NGSmgxeDA"> links2 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcveGpVZkdoUDBpdWs"> links3 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvaHdiUW1kR19vSUk"> links4 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvdXR3d21fVmtXaGM"> links5 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvLW5SY1NPd254b3M"> links6 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvTGRleG5NRmc4Z0E"> links7 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvQ2hFVlJXb1VWSHc"> links8 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvMzlHTFpNQTNIR0k"> links9 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvUjRaUHVkUmNrcXc"> links10 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvOWw3Q1ZVRjFKMzA"> links1  </a>
+
+<!--###-------------------events.html-----------------------------------------------------###-->
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvMC1ZSTE4QjFnSGs" >links </a>
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvTW4tZzk3aXN0bmM" >links1 </a>
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvUnFPT2UzTGdsSGM" >links2 </a>
+
+<!--###-------------------farenheit.html-----------------------------------------------------###-->
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvTUpDaGRnTF9yQlU"> links</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvMjZHSzNXbXNWLVU"> links</a>
+
+<!--###---------------------function.html---------------------------------------------------###-->
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvaW9LeXdSVUdsQ0E">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvOVlQbmJUMTNVN1k">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvMUpZZElBcTQ4LUk">link</a>
+
+<!--###----------------globa.html--------------------------------------------------------###-->
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvcVo3b2hmVlJTT0E">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvZEM5Sk8wWmpITFU">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvOU5yR2o4QjZCUlU">link</a>
+
+<!--###--------------inaccurate.html----------------------------------------------------------###-->
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvejg2UlJtc0dOd2c">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvR0pBVUEyQ2xxRnc">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcveExGNE9uV3VQTEk">link</a>
+
+<!--###----------------infinity.html--------------------------------------------------------###-->
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvQ3dRcjVEUFlZZUk">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvR0dCQVc1Slpva3M">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvTjFxa2NuZFZCdmM">link</a>
+
+<!--###-----------------intro.html-------------------------------------------------------###-->
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvcjdqZV9weENxbTA">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvYk9IR2ZralV6TXM">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvVDAxRmVybHhJWGc">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvMnNzLUxmcUJwRk0">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvVFZOZ3RBV0tlOXM">link</a>
+
+<!--###-----------------math.html-------------------------------------------------------###-->
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvaHZCWENhdlZPemM">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvb2twQ2lBTmdvRm8">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvREVwTWZid2FDVDg">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvMVpFRC14enh2Znc">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvVmtyTzJtWkxpQ1k">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvSFVnX1dMem4wTVk">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvWEtTbXBUWC1qY1k">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvOEZHd2tBN3k5ZnM">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvS1l0OE9aVWpfM28">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcva3VkN3Znb19BV2M">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvSzdBdU1ERVQ1VFU">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvLVJqb3AyS2dJcFU">link</a>
+
+<!--###---------------nav.html---------------------------------------------------------###-->
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvQ3pYdEo0UzcydE0">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvMkVpMVRwWlBodms">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvSkNFLXo0YVFGbTQ">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvazVqNFk1LU05QzA">link</a>
+
+<!--###----------------number.html--------------------------------------------------------###-->
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvU19jSF9yUGc0bkk">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcveExyb3cxd2wzckE">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvbm5TaXNTRlprRVE">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvRFhMa2hhVkVvR2c">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvOHJ5TmNEX0Z0MEk">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvQjNaVGRaRzNFRWM">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvb3dtenFCSHE1M3c">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvcVltbWpIOEJCblU">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvYXJUR0IwTHVrRkU">link</a>
+
+<!--###-----------------objets.html-------------------------------------------------------###-->
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvQ0w1QzhxZ0xmOVE">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvRWVfMC1Ca0dGeFU">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvcVRuSVNFdkdXNVE">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvRHRTam91blpMZ1k">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvdzJjTXZ3VUZwVGc">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvQ2VQbTBZeVZ3ZkE">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvajItX0ZDUS1Cblk">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvSEpJWXFqU0xwSVE">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvQ1NJWGFPWERzLTg">link</a>
+
+<!--###---------------oper.html---------------------------------------------------------###-->
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcveUxuVlZBUndhajg">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvTHBlcnBta0tKU2c">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvZ2djUGl5MjljVEU">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvM0VaSzFRWG1KVGs">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvY1pCdGlnR3N6dDA">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvdEw0bHdPMi1ZQkE">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvNl92bzRibVBQVXM">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvdEdvcXZCUW1LQ1U">link</a>
+
+<!--###---------------output.html---------------------------------------------------------###-->
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvVDI3bjNOZnRKeVU">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvQzZ5YkNjT2o2Um8">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvUzNiY25oR21wdTg">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvTWZYaEVtZHZWTXM">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvUTB4TzRYcHNydTQ">link</a>
+
+<!--###-----------------random.html-------------------------------------------------------###-->
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcveDNWNVJfeEc5R0U">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcveTAzbWNxUl9icHM">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvdWg0YV9mZUxxaWM">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvOHZSR2dXR2xNREE">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvZFcyUXFTcTRJVmM">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcveEpxS2xneVZnaGs">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvVFlEYkpUY2ZDOFk">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvbm9hWXZmc1pnRW8">link</a>
+
+<!--###-----------------scope.html-------------------------------------------------------###-->
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvVTRkem1lbEE4SU0">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvQkpCWnUxSE9yNGc">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvYkoyMi1YM2pSZlk">link</a>
+
+<!--###----------------statements.html--------------------------------------------------------###-->
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvQmp4QzFENkZJQ0k">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvY3FEMFc5VnZJWjg">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvMmVrdDhrdFZSVkk">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvc1VJQjhLYkJqSVU">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvS0xTUFZFMWFwb1E">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcveWxJalhpbVRkV3c">link</a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvRl81anFhcFpNZUk">link</a>
+
+<!--###------------------syntaxis.html------------------------------------------------------###-->
+<a hreft="https://drive.google.com/open?id=0BykGOP-LhVcvUW5aV0VzUjlhN3M">sxript</a> 
+<a hreft="https://drive.google.com/open?id=0BykGOP-LhVcvRGZlVERBcHhVSnc">sxript</a> 
+<a hreft="https://drive.google.com/open?id=0BykGOP-LhVcvQ09yRC1hSjhOYUk">sxript</a> 
+<a hreft="https://drive.google.com/open?id=0BykGOP-LhVcvN3Z5MmM1ZVYxSkE">sxript</a> 
+<a hreft="https://drive.google.com/open?id=0BykGOP-LhVcvQ09yVTc0d1o5MDg">sxript</a> 
+<a hreft="https://drive.google.com/open?id=0BykGOP-LhVcvempCTS1pSVZhS2s">sxript</a> 
+<a hreft="https://drive.google.com/open?id=0BykGOP-LhVcvanNnYmFuWi1ZbVE">sxript</a> 
+<a hreft="https://drive.google.com/open?id=0BykGOP-LhVcvcktMMU11OVB3UEE">sxript</a> 
+<a hreft="https://drive.google.com/open?id=0BykGOP-LhVcvZngySDZfRUN2MW8">sxript</a> 
+<a hreft="https://drive.google.com/open?id=0BykGOP-LhVcvSlZnY0xXelVVVWs">sxript</a> 
+<a hreft="https://drive.google.com/open?id=0BykGOP-LhVcvQXlPcU9HYmtROVU">sxript</a>
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvNmlHYmdiSVB4a00">sxript</a>
+
+<!--###----------------variable.html--------------------------------------------------------###-->
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvejRzcDhhMnljS2M" > links </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvdWdQaF9tZng2Sk0" > links1 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvRmJwRjFzVkUxSFk" > links2 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvSmR6dVQwTlF5ZlU" > links3 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcveE85SlE3OHhHajg" > links4 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcveU15WkJmYll2THM" > links5 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvTHlmMElsUEpOTDA" > links6 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvVGEtOE9QMUVNUDg" > links7 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvZXpjMDM5VzlVakk" > links8 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvQ01Ud0hLS2xrb3M" > links9 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvdEQ5T2xFdzRBYXM" > links10 </a>
+<a href="https://drive.google.com/open?id=0BykGOP-LhVcvMlA2WV83STN4UlU" > links11 </a>
+
+<!--###-----------------wherwto.html-------------------------------------------------------###-->
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvMEJoUEd1RnpDdjA " > link </a>
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvMy1FM0xQa0lzcHM " > link1 </a>
+<a href=" https://drive.google.com/open?id=0BykGOP-LhVcvODhQTDZ1ZElMa3c " > link2 </a>
+
+<!--###------------------script.html------------------------------------------------------###-->
+
+
+
